bf77a9b0aba8e35dcd5710a990e9af4f20376727
Andres Copeticona <andrescopeticona@outlook.es>
2023-07-23 19:15:50 -0400

    update coop

--- src/ecuations/coop.ts
+++ src/ecuations/coop.ts
@@ -1,7 +1,6 @@
-import Plotly from "plotly.js-dist";
-import { $in } from "../lib/index"
+import { $in, drawPlot } from "../lib/index"
 
-export function Coop() {
+function calculateData() {
   let q1 = parseFloat($in('Q1').value);
   let q2 = parseFloat($in('Q2').value);
   const k5 = 0.01 * parseFloat($in('K1').value);
@@ -10,69 +9,69 @@ export function Coop() {
   const k4 = 0.01 * parseFloat($in('K4').value);
   const maxTime = parseInt($in("T").value);
 
-  function draw() {
-    let k1 = 0.08;
-    let k2 = 0.04;
-    let k7 = 0.002;
-    let k8 = 0.002;
-    let t = 0;
-    let ti = 0;
-    let qi1 = q1;
-    let qi2 = q2;
+  let k1 = 0.08;
+  let k2 = 0.04;
+  let k7 = 0.002;
+  let k8 = 0.002;
+  let t = 0;
+  let ti = 0;
+  let qi1 = q1;
+  let qi2 = q2;
 
-    const tValues = [];
-    const q1Values = [];
-    const q2Values = [];
+  const tValues = [];
+  const q1Values = [];
+  const q2Values = [];
 
-    while (t < maxTime) {
-      tValues.push(t);
+  while (t < maxTime) {
+    tValues.push(t);
 
-      let r = 1 / (1 + k1 * q1 * q2 - k2 * q1 * q2);
-      if (r < 0) {
-        r = 0;
-      }
-
-      const dq1 = k5 * r * q1 * q2 - k7 * q1 * q2 - k3 * q1;
-      const dq2 = k6 * r * q1 * q2 - k8 * q1 * q2 - k4 * q2;
-
-      q1 += dq1;
-      q2 += dq2;
-
-      q1Values.push(q1);
-      q2Values.push(q2);
-
-      t += 1;
-      ti = t;
-      qi1 = q1;
-      qi2 = q2;
+    let r = 1 / (1 + k1 * q1 * q2 - k2 * q1 * q2);
+    if (r < 0) {
+      r = 0;
     }
 
-    const trace1 = {
-      x: tValues,
-      y: q1Values,
-      type: 'scatter',
-      name: 'Q1',
-      line: { color: 'green' }
-    };
+    const dq1 = k5 * r * q1 * q2 - k7 * q1 * q2 - k3 * q1;
+    const dq2 = k6 * r * q1 * q2 - k8 * q1 * q2 - k4 * q2;
 
-    const trace2 = {
-      x: tValues,
-      y: q2Values,
-      type: 'scatter',
-      name: 'Q2',
-      line: { color: 'red' }
-    };
+    q1 += dq1;
+    q2 += dq2;
 
-    const data = [trace1, trace2];
+    q1Values.push(q1);
+    q2Values.push(q2);
 
-    const layout = {
-      title: 'Existencia Cooperativa',
-      xaxis: { title: 'Time (t)' },
-      yaxis: { title: 'Population' },
-    };
-
-    Plotly.newPlot('plot', data, layout, { responsive: true });
+    t += 1;
+    ti = t;
+    qi1 = q1;
+    qi2 = q2;
   }
 
-  draw();
+  const trace1 = {
+    x: tValues,
+    y: q1Values,
+    type: 'scatter',
+    name: 'Población Q1',
+    line: { color: 'green' }
+  };
+
+  const trace2 = {
+    x: tValues,
+    y: q2Values,
+    type: 'scatter',
+    name: 'Población Q2',
+    line: { color: 'red' }
+  };
+
+  return [trace1, trace2];
+}
+
+export function Coop() {
+  const data = calculateData();
+
+  const layout = {
+    title: 'Existencia Cooperativa',
+    xaxis: { title: 'Time (t)' },
+    yaxis: { title: 'Population' },
+  };
+
+  drawPlot({ data, layout });
 }
