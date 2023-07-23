fd60ac68921d2fd8c714b6d8c6fdcdc4db5938e4
Andres Copeticona <andrescopeticona@outlook.es>
2023-07-23 19:18:49 -0400

    change layout legend

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,13 @@ import Plotly from "plotly.js-dist";
 
 export const $in = (id: string) => document.getElementById(id) as HTMLInputElement
 
-export const drawPlot = ({ data, layout }) =>
+export const drawPlot = ({ data, layout }) => {
+  layout = {
+    ...layout, legend: {
+      x: 0,
+      y: 1,
+    }
+  };
+
   Plotly.newPlot('plot', data, layout, { responsive: true });
+}
