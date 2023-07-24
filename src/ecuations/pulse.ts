7257f8aa1d1d41828cd10737a1631c37961b6b3c
Andres Copeticona <andrescopeticona@outlook.es>
2023-07-24 16:19:49 -0400

    improve pulse

--- src/ecuations/pulse.ts
+++ src/ecuations/pulse.ts
@@ -1,35 +1,35 @@
 import { drawPlot, $in } from "src/lib";
 
 function calculateData() {
-  var TM = parseFloat($in("TM").value);
-  var K2 = 0.01 * parseFloat($in("K2").value);
-  var K4 = 0.01 * parseFloat($in("K4").value);
+  let TM = parseFloat($in("TM").value);
+  let K2 = 0.01 * parseFloat($in("K2").value);
+  let K4 = 0.01 * parseFloat($in("K4").value);
+  const maxTime = parseInt($in('T').value);
 
-  var t = 0;
-  var ti = 0;
-  var e = 1;
-  var q = 2;
-  var c = 2;
-  var k1 = 0.02;
-  var k3 = 0.0003;
-  var f1 = 0.05;
-  var f2 = 0.1;
-  var q0 = 1;
-  var c0 = 1;
-  var t0 = 2;
-  var dt = 0.5;
+  let t = 0;
+  let e = 1;
+  let q = 2;
+  let c = 2;
+  let k1 = 0.02;
+  let k3 = 0.0003;
+  let f1 = 0.05;
+  let f2 = 0.1;
+  let q0 = 1;
+  let c0 = 1;
+  let t0 = 2;
+  let dt = 0.5;
 
-  var tiempo = [];
-  var valorQ = [];
-  var valorC = [];
+  let tiempo = [];
+  let valorQ = [];
+  let valorC = [];
 
   do {
-    var m = TM - f1 * q - f2 * c;
-    var dq = k1 * e * m - K2 * q - k3 * c * c * q;
-    var dc = K2 * q + k3 * q * c * c - K4 * c;
-    var qi = q + dq * dt;
-    var ci = c + dc * dt;
-    var t1 = t + dt;
+    let m = TM - f1 * q - f2 * c;
+    let dq = k1 * e * m - K2 * q - k3 * c * c * q;
+    let dc = K2 * q + k3 * q * c * c - K4 * c;
+    let qi = q + dq * dt;
+    let ci = c + dc * dt;
+    let t1 = t + dt;
 
     tiempo.push(t * t0);
     valorQ.push(q * q0);
@@ -39,7 +39,7 @@ function calculateData() {
     q = qi;
     c = ci;
 
-  } while ((t * t0) <= 320);
+  } while ((t * t0) <= maxTime);
 
   return [
     { x: tiempo, y: valorQ, type: 'scatter', name: 'Q', line: { color: 'blue' } },
